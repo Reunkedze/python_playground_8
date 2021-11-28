a5f95ed230e05b8d7f3b99851e0e32c275cb7618
Сергей Реунок <69862026+Reunkedze@users.noreply.github.com>
2021-11-28 05:14:26 +0300

    Add files via upload

---
+++ task6/library.py
@@ -0,0 +1,16 @@
+# meaningless function
+def functiondunction():
+    return None
+
+
+def fuctionshmunction(param, pam_pam):
+    print('This is Antoshka\'s song words')
+
+
+def whyamidoingthis():
+    print('I hope they will leave me a scholarship')
+
+
+# i forgot a comments
+def letscheckwhatcomesoutofit():
+    return 'I tired:0'
